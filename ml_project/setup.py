8e71907ba5fb33d94f3c5b3c40891d2ae3627635
Mikhail Korotkov <korotkov@hypeauditor.com>
2021-04-28 18:32:38 +0300

    Adding setup.py

---
+++ ml_project/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ml_project",
+    packages=find_packages(),
+    version="1.1.0",
+    description="Example of ml project",
+    author="Mikhail Korotkov",
+    install_requires=[
+        "dataclasses==0.8",
+        "marshmallow-dataclass==8.4.1",
+        "pyyaml==5.4.1",
+        "click==7.1.2",
+        "pandas==1.2.4",
+        "scikit-learn==0.24.1",
+        "python-dotenv>=0.5.1",
+    ],
+    license="MIT",
+)
