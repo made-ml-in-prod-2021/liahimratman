1eb0e25190e13eed264a527a1a59e8a5dc6fafdb
Mikhail Korotkov <korotkov@hypeauditor.com>
2021-04-29 19:33:26 +0300

    Fit predict model

---
+++ ml_project/models/model_fit_predict.py
@@ -0,0 +1,58 @@
+import pickle
+from typing import Dict, Union
+import numpy as np
+import pandas as pd
+
+from sklearn.ensemble import RandomForestClassifier#, RandomForestRegressor
+from sklearn.linear_model import LogisticRegression#, LinearRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, jaccard_score, f1_score, roc_auc_score
+
+from ml_project.params.train_params import TrainingParams
+
+
+SklearnClassificationModel = Union[RandomForestClassifier, LogisticRegression]
+
+
+def train_model(
+    features: pd.DataFrame, target: pd.Series, train_params: TrainingParams
+) -> SklearnClassificationModel:
+    if train_params.model_type == "RandomForestClassifier":
+        model = RandomForestClassifier(
+            n_estimators=100, random_state=train_params.random_state
+        )
+    elif train_params.model_type == "LogisticRegression":
+        model = LogisticRegression(random_state=train_params.random_state)
+    else:
+        raise NotImplementedError()
+    model.fit(features, target)
+
+    return model
+
+
+def predict_model(
+        model: SklearnClassificationModel, features: pd.DataFrame
+) -> np.ndarray:
+    predicts = model.predict(features)
+
+    return predicts
+
+
+def evaluate_model(
+    predicts: np.ndarray, target: pd.Series
+) -> Dict[str, float]:
+
+    return {
+        "accuracy": accuracy_score(target, predicts),
+        "precision": precision_score(target, predicts),
+        "recall": recall_score(target, predicts),
+        "jaccard": jaccard_score(target, predicts),
+        "f1": f1_score(target, predicts),
+        "roc_auc_score": roc_auc_score(target, predicts),
+    }
+
+
+def serialize_model(model: SklearnClassificationModel, output: str) -> str:
+    with open(output, "wb") as f:
+        pickle.dump(model, f)
+
+    return output
