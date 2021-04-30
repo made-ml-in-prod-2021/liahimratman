033b31387a4d6f5a760c864204b040a218ffa421
Mikhail Korotkov <korotkov@hypeauditor.com>
2021-04-30 12:06:21 +0300

    Fix train params and train config

--- ml_project/params/train_params.py
+++ ml_project/params/train_params.py
@@ -4,4 +4,5 @@ from dataclasses import dataclass, field
 @dataclass()
 class TrainingParams:
     model_type: str = field(default="LinearRegression")
+    max_iter: int = field(default=1000)
     random_state: int = field(default=100)
