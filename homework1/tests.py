cd75278c4b96544874cb08efef4a52913e84f9f5
Mikhail Korotkov <korotkov@hypeauditor.com>
2021-05-03 01:56:20 +0300

    Fix tests

--- homework1/tests.py
+++ homework1/tests.py
@@ -7,7 +7,6 @@ from py._path.local import LocalPath
 from sklearn.linear_model import LogisticRegression
 from pathlib import Path
 from faker import Faker
-from hydra.utils import to_absolute_path
 import pytest
 
 from ml_project.data_functions.make_dataset import read_data, split_train_val_data
@@ -22,12 +21,12 @@ from train_pipeline import train_pipeline
 
 @pytest.fixture()
 def dataset_path():
-    return to_absolute_path("tests_data/train_data_sample.csv")
+    return "homework1/tests_data/train_data_sample.csv"
 
 
 @pytest.fixture()
 def fake_dataset_path():
-    return to_absolute_path("tests_data/fake_dataset.csv")
+    return "homework1/tests_data/fake_dataset.csv"
 
 
 @pytest.fixture()
