b0ade726f07798e0e1e057f5455e1971042aec6b
Mikhail Korotkov <korotkov@hypeauditor.com>
2021-05-03 02:43:07 +0300

    Fix pylint

--- homework1/ml_project/transformers/make_transformers.py
+++ homework1/ml_project/transformers/make_transformers.py
@@ -4,7 +4,7 @@ from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.preprocessing._data import _handle_zeros_in_scale
 from sklearn.utils.extmath import _incremental_mean_and_var
 from sklearn.utils.validation import (check_is_fitted,
-                                FLOAT_DTYPES)
+                                      FLOAT_DTYPES)
 
 
 class StandardScalerTransformer(TransformerMixin, BaseEstimator):
@@ -34,21 +34,21 @@ class StandardScalerTransformer(TransformerMixin, BaseEstimator):
             del self.mean_
             del self.var_
 
-    def fit(self, x, mean=None, scale=None):
+    def fit(self, data, mean=None, scale=None):
         """
         Fir transformer
-        :param x: data
+        :param data: data
         :param mean: sample means
         :param scale: sample scales
         :return: fitted transformer
         """
         self._reset()
-        return self.partial_fit(x, mean, scale)
+        return self.partial_fit(data, mean, scale)
 
-    def partial_fit(self, x, mean, scale):
+    def partial_fit(self, data, mean, scale):
         """
         Fit transformer
-        :param x: data
+        :param data: data
         :param mean: sample means
         :param scale: sample scales
         :return: fitted transformer
@@ -58,16 +58,16 @@ class StandardScalerTransformer(TransformerMixin, BaseEstimator):
             self.scale_ = scale
         else:
             first_call = not hasattr(self, "n_samples_seen_")
-            x = self._validate_data(x, estimator=self, dtype=FLOAT_DTYPES,
-                                    force_all_finite='allow-nan', reset=first_call)
-            n_features = x.shape[1]
+            data = self._validate_data(data, estimator=self, dtype=FLOAT_DTYPES,
+                                       force_all_finite='allow-nan', reset=first_call)
+            n_features = data.shape[1]
             dtype = np.int64
 
             if not hasattr(self, 'n_samples_seen_'):
                 self.n_samples_seen_ = np.zeros(n_features, dtype=dtype)
             elif np.size(self.n_samples_seen_) == 1:
                 self.n_samples_seen_ = np.repeat(
-                    self.n_samples_seen_, x.shape[1])
+                    self.n_samples_seen_, data.shape[1])
                 self.n_samples_seen_ = \
                     self.n_samples_seen_.astype(dtype, copy=False)
 
@@ -75,7 +75,7 @@ class StandardScalerTransformer(TransformerMixin, BaseEstimator):
                 self.mean_ = .0
                 self.var_ = .0
             self.mean_, self.var_, self.n_samples_seen_ = \
-                _incremental_mean_and_var(x, self.mean_, self.var_,
+                _incremental_mean_and_var(data, self.mean_, self.var_,
                                           self.n_samples_seen_)
 
             if np.ptp(self.n_samples_seen_) == 0:
@@ -85,20 +85,20 @@ class StandardScalerTransformer(TransformerMixin, BaseEstimator):
 
         return self
 
-    def transform(self, x, copy=None):
+    def transform(self, data, copy=None):
         """
         Transform data
-        :param x: data
+        :param data: data
         :param copy: copy flag
         :return: transformed data
         """
         check_is_fitted(self)
 
         copy = copy if copy is not None else self.copy
-        x = self._validate_data(x, copy=copy, reset=False,
-                                estimator=self, dtype=FLOAT_DTYPES,
-                                force_all_finite='allow-nan')
-        x -= self.mean_
-        x /= self.scale_
+        data = self._validate_data(data, copy=copy, reset=False,
+                                   estimator=self, dtype=FLOAT_DTYPES,
+                                   force_all_finite='allow-nan')
+        data -= self.mean_
+        data /= self.scale_
 
-        return x
+        return data
