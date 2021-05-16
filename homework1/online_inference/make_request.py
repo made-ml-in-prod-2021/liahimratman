1c2d02f2816757f7d4d0d3d567069655a57e96fd
Mikhail Korotkov <ma-korotkov@yandex.ru>
2021-05-16 23:13:17 +0300

    Fix localhost

--- homework1/online_inference/make_request.py
+++ homework1/online_inference/make_request.py
@@ -8,7 +8,7 @@ DATA_PATH = "ml_project/data/heart.csv"
 if __name__ == '__main__':
     data = pd.read_csv(DATA_PATH)
     response = requests.post(
-        "http://127.0.0.1:8000/predict",
+        "http://localhost:8000/predict",
         json={
             "ids": list(range(data.shape[0])),
             "features": data.values.tolist(),
